8ac7b22a36730603c3e38e396e4974a9b45fc96b
nmx <2351050106nam@ou.edu.vn>
2026-05-22 23:21:34 +0700

    Add files via upload

---
+++ GUI_QLSV/SinhVien/FrmDiem.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace GUI_QLSV
+{
+    partial class FrmDiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbHocKy = new System.Windows.Forms.ComboBox();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.dgvDiem = new System.Windows.Forms.DataGridView();
+            this.lblTongTinChi = new System.Windows.Forms.Label();
+            this.lblDiemTB = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Xem điểm: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Học kỳ: ";
+            // 
+            // cbHocKy
+            // 
+            this.cbHocKy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbHocKy.FormattingEnabled = true;
+            this.cbHocKy.Location = new System.Drawing.Point(210, 108);
+            this.cbHocKy.Name = "cbHocKy";
+            this.cbHocKy.Size = new System.Drawing.Size(121, 24);
+            this.cbHocKy.TabIndex = 2;
+            // 
+            // btnXem
+            // 
+            this.btnXem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXem.Location = new System.Drawing.Point(468, 98);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(110, 34);
+            this.btnXem.TabIndex = 3;
+            this.btnXem.Text = "Xem điểm";
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // dgvDiem
+            // 
+            this.dgvDiem.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDiem.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDiem.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dgvDiem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDiem.Location = new System.Drawing.Point(13, 225);
+            this.dgvDiem.Name = "dgvDiem";
+            this.dgvDiem.ReadOnly = true;
+            this.dgvDiem.RowHeadersVisible = false;
+            this.dgvDiem.RowHeadersWidth = 51;
+            this.dgvDiem.RowTemplate.Height = 24;
+            this.dgvDiem.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDiem.Size = new System.Drawing.Size(940, 373);
+            this.dgvDiem.TabIndex = 4;
+            // 
+            // lblTongTinChi
+            // 
+            this.lblTongTinChi.AutoSize = true;
+            this.lblTongTinChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTongTinChi.Location = new System.Drawing.Point(181, 534);
+            this.lblTongTinChi.Name = "lblTongTinChi";
+            this.lblTongTinChi.Size = new System.Drawing.Size(150, 25);
+            this.lblTongTinChi.TabIndex = 5;
+            this.lblTongTinChi.Text = "Tổng tín chỉ: 0";
+            // 
+            // lblDiemTB
+            // 
+            this.lblDiemTB.AutoSize = true;
+            this.lblDiemTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiemTB.Location = new System.Drawing.Point(572, 534);
+            this.lblDiemTB.Name = "lblDiemTB";
+            this.lblDiemTB.Size = new System.Drawing.Size(183, 22);
+            this.lblDiemTB.TabIndex = 6;
+            this.lblDiemTB.Text = "Điểm TB hệ 4: 0.00";
+            // 
+            // FrmDiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 594);
+            this.Controls.Add(this.lblDiemTB);
+            this.Controls.Add(this.lblTongTinChi);
+            this.Controls.Add(this.dgvDiem);
+            this.Controls.Add(this.btnXem);
+            this.Controls.Add(this.cbHocKy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmDiem";
+            this.Text = "FrmDiem";
+            this.Load += new System.EventHandler(this.btnXem_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbHocKy;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.DataGridView dgvDiem;
+        private System.Windows.Forms.Label lblTongTinChi;
+        private System.Windows.Forms.Label lblDiemTB;
+    }
+}
---
+++ GUI_QLSV/SinhVien/FrmDiem.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+
+namespace GUI_QLSV
+{
+    public partial class FrmDiem : Form
+    {
+        private string maSV;
+        string connectionString =
+            @"Data Source=DESKTOP-7BJDSV2\NGOC;Initial Catalog=QLSV_OU;Integrated Security=True";
+
+        public FrmDiem(string maSV)
+        {
+            InitializeComponent();
+
+            this.maSV = maSV;
+
+            this.Load += FrmDiem_Load;
+        }
+
+        private void FrmDiem_Load(object sender, EventArgs e)
+        {
+            CaiDatGrid();
+            LoadHocKy();
+            LoadDiem();
+        }
+        private void CaiDatGrid()
+        {
+            dgvDiem.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvDiem.AllowUserToAddRows = false;
+            dgvDiem.ReadOnly = true;
+            dgvDiem.RowHeadersVisible = false;
+            dgvDiem.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            dgvDiem.BackgroundColor = Color.White;
+            dgvDiem.GridColor = Color.LightGray;
+
+            dgvDiem.ColumnHeadersDefaultCellStyle.Font =
+                new Font("Segoe UI", 10, FontStyle.Bold);
+
+            dgvDiem.DefaultCellStyle.Font =
+                new Font("Segoe UI", 10);
+
+            dgvDiem.RowTemplate.Height = 30;
+        }
+        private void LoadHocKy()
+        {
+            cbHocKy.Items.Clear();
+            cbHocKy.Items.Add("Tất cả");
+
+            string sql = @"
+                SELECT DISTINCT TenHocKy + ' - ' + NamHoc AS HocKy
+                FROM vw_BangDiem_ChiTiet
+                WHERE MaSV = @MaSV
+                ORDER BY HocKy
+            ";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@MaSV", maSV);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                cbHocKy.Items.Add(reader["HocKy"].ToString());
+                            }
+                        }
+                    }
+                }
+
+                cbHocKy.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải học kỳ: " + ex.Message);
+            }
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            LoadDiem();
+        }
+        private void LoadDiem()
+        {
+            string sql = @"
+                SELECT
+                    TenMH AS [Môn học],
+                    SoTinChi AS [Số tín chỉ],
+                    TenHocKy AS [Học kỳ],
+                    NamHoc AS [Năm học],
+                    DiemGK AS [Điểm GK],
+                    DiemCK AS [Điểm CK],
+                    DiemTK AS [Điểm TK],
+                    DiemChu AS [Điểm chữ],
+                    DiemHe4 AS [Điểm hệ 4],
+                    KetQua AS [Kết quả]
+                FROM vw_BangDiem_ChiTiet
+                WHERE MaSV = @MaSV
+            ";
+
+            if (cbHocKy.Text != "Tất cả")
+            {
+                sql += " AND TenHocKy + ' - ' + NamHoc = @HocKy";
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@MaSV", maSV);
+
+                        if (cbHocKy.Text != "Tất cả")
+                        {
+                            cmd.Parameters.AddWithValue("@HocKy", cbHocKy.Text);
+                        }
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        dgvDiem.DataSource = dt;
+
+                        TinhTongKet(dt);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải điểm: " + ex.Message);
+            }
+        }
+
+        private void TinhTongKet(DataTable dt)
+        {
+            decimal tongTinChi = 0;
+            decimal tongDiemHe4 = 0;
+
+            foreach (DataRow row in dt.Rows)
+            {
+                if (row["Số tín chỉ"] != DBNull.Value &&
+                    row["Điểm hệ 4"] != DBNull.Value)
+                {
+                    decimal soTinChi = Convert.ToDecimal(row["Số tín chỉ"]);
+                    decimal diemHe4 = Convert.ToDecimal(row["Điểm hệ 4"]);
+
+                    tongTinChi += soTinChi;
+                    tongDiemHe4 += soTinChi * diemHe4;
+                }
+            }
+
+            lblTongTinChi.Text = "Tổng tín chỉ: " + tongTinChi;
+
+            if (tongTinChi > 0)
+            {
+                decimal diemTB = tongDiemHe4 / tongTinChi;
+                lblDiemTB.Text = "Điểm TB hệ 4: " + Math.Round(diemTB, 2);
+            }
+            else
+            {
+                lblDiemTB.Text = "Điểm TB hệ 4: 0.00";
+            }
+        }
+    }
+}
